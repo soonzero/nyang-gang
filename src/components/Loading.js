f3c89863a937116638b5a0b2dbaff45387dc918c
soonzero <sykeum0912@naver.com>
2022-05-07 01:46:32 +0900

    style: Loading styled-components  분리

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,13 @@
+import { LoadingStyle } from "./styled";
 import { ReactComponent as Spinner } from "images/spinner.svg";
 
-export default function Loading() {
+export default function Loading(props) {
   return (
-    <>
+    <LoadingStyle modal={props.modal}>
       <div className="loading">
         <Spinner />
         🐱 목록을 불러오고 있어요! 조금만 기다려주세요! 🐶
       </div>
-    </>
+    </LoadingStyle>
   );
 }
