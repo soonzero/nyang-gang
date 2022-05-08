3d48fa994d6befff8e2d4a551e0d8eef99393279
soonzero <sykeum0912@naver.com>
2022-05-08 21:41:26 +0900

    docs: 캐러셀 link 연결

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -27,7 +27,7 @@ const array = [
       "https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_1280.jpg",
     title: "반려동물 등록은 필수입니다",
     text: "반려동물 등록",
-    link: "/",
+    link: "/license",
   },
 ];
 
