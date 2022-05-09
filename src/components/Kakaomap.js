2ca2d39b11a89ac62274dd6c0e497f7436735d83
soonzero <sykeum0912@naver.com>
2022-05-09 13:13:40 +0900

    add: 마커 이미지 변경

--- src/components/Kakaomap.js
+++ src/components/Kakaomap.js
@@ -26,6 +26,16 @@ export default function Kakaomap(props) {
     }
 
     if (props.hospital && props.data) {
+      const imageSrc =
+        "https://firebasestorage.googleapis.com/v0/b/nyang-gang.appspot.com/o/hospital.png?alt=media&token=5087021d-9b96-4f8b-a209-c038aa181d1c";
+      const imageSize = new kakao.maps.Size(30, 45);
+      const imageOption = { offset: new kakao.maps.Point(15, 45) };
+      const markerImg = new kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
+      );
+
       props.data
         .filter((d) => d.BSN_STATE_NM !== "폐업")
         .map((d) => {
@@ -36,6 +46,7 @@ export default function Kakaomap(props) {
           const mark = new kakao.maps.Marker({
             map: map,
             position: coords,
+            image: markerImg,
           });
           const iwContent = `<div style="padding: 10px; text-align: center; width: 100%; height: 100%;">
         ${d.BIZPLC_NM}
@@ -52,6 +63,15 @@ export default function Kakaomap(props) {
           });
         });
     } else if (props.shelter && props.data) {
+      const imageSrc =
+        "https://firebasestorage.googleapis.com/v0/b/nyang-gang.appspot.com/o/shelter.png?alt=media&token=38feffab-3e16-492f-9e0e-41398fe96ef3";
+      const imageSize = new kakao.maps.Size(30, 48);
+      const imageOption = { offset: new kakao.maps.Point(15, 48) };
+      const markerImg = new kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
+      );
       props.data.map((d) => {
         const coords = new kakao.maps.LatLng(
           d.REFINE_WGS84_LAT,
@@ -60,6 +80,7 @@ export default function Kakaomap(props) {
         const mark = new kakao.maps.Marker({
           map: map,
           position: coords,
+          image: markerImg,
         });
         const iwContent = `<div style="text-align: center; width: 100%; height: 100%;">
         ${d.ENTRPS_NM}
@@ -87,6 +108,40 @@ export default function Kakaomap(props) {
         });
         const iwContent = `<div style="text-align: center; width: 100%; height: 100%;">
         ${d.DIV_NM}
+        </div>`;
+
+        const infoWindow = new kakao.maps.InfoWindow({
+          content: iwContent,
+        });
+        kakao.maps.event.addListener(mark, "mouseover", function () {
+          infoWindow.open(map, mark);
+        });
+        kakao.maps.event.addListener(mark, "mouseout", function () {
+          infoWindow.close();
+        });
+      });
+    } else if (props.pharmacy && props.data) {
+      const imageSrc =
+        "https://firebasestorage.googleapis.com/v0/b/nyang-gang.appspot.com/o/pharmacy.png?alt=media&token=580dbdb8-befe-4011-901a-c5416a2ece20";
+      const imageSize = new kakao.maps.Size(30, 45);
+      const imageOption = { offset: new kakao.maps.Point(15, 45) };
+      const markerImg = new kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
+      );
+      props.data.map((d) => {
+        const coords = new kakao.maps.LatLng(
+          d.REFINE_WGS84_LAT,
+          d.REFINE_WGS84_LOGT
+        );
+        const mark = new kakao.maps.Marker({
+          map: map,
+          position: coords,
+          image: markerImg,
+        });
+        const iwContent = `<div style="text-align: center; width: 100%; height: 100%;">
+        ${d.BIZPLC_NM}
         </div>`;
 
         const infoWindow = new kakao.maps.InfoWindow({
