71fd1f43793722b2676439eb16030dab53eeace8
soonzero <sykeum0912@naver.com>
2022-05-12 00:34:45 +0900

    feat: 유기동물 패널 api 연동

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,10 +3,10 @@ import { ReactComponent as Spinner } from "images/spinner.svg";
 
 export default function Loading(props) {
   return (
-    <LoadingStyle modal={props.modal}>
+    <LoadingStyle modal={props.modal} notext={props.notext}>
       <div className="loading">
         <Spinner />
-        🐱 목록을 불러오고 있어요! 조금만 기다려주세요! 🐶
+        {!props.notext && `🐱 목록을 불러오고 있어요! 조금만 기다려주세요! 🐶`}
       </div>
     </LoadingStyle>
   );
--- src/components/TodayPanel.js
+++ src/components/TodayPanel.js
@@ -1,11 +1,68 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Loading from "./Loading";
 import { TodayPanelStyle } from "./styled";
 
 export default function TodayPanel() {
+  let didCancel;
+  const API_KEY = process.env.REACT_APP_PUB_DATA_API_KEY;
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    setIsLoading(true);
+    try {
+      const index = Math.ceil(Math.random() * 10);
+      const result = await axios({
+        method: "GET",
+        url: `https://openapi.gg.go.kr/AbdmAnimalProtect?KEY=${API_KEY}&Type=json&pIndex=1&pSize=15&STATE_NM=보호중`,
+      });
+      if (!didCancel) {
+        console.log(
+          result.data.AbdmAnimalProtect[1].row.filter(
+            (d) => d.IMAGE_COURS != undefined
+          )
+        );
+        setData(
+          result.data.AbdmAnimalProtect[1].row.filter(
+            (d) => d.IMAGE_COURS != undefined
+          )[index]
+        );
+        setIsLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+
+    return () => {
+      didCancel = false;
+    };
+  }, []);
+
   return (
     <TodayPanelStyle>
-      <h2 className="title">가족을 기다리고 있어요</h2>
-      <span className="img"></span>
-      <p className="desc">현재 --에서 보호 중</p>
+      {!isLoading ? (
+        <div
+          className="animal"
+          onClick={() => navigate(`/abandoned/${data.ABDM_IDNTFY_NO}`)}
+        >
+          <h2 className="title">가족을 기다리고 있어요</h2>
+          <span
+            className="img"
+            style={{ backgroundImage: `url(${data.IMAGE_COURS})` }}
+          ></span>
+          <p className="desc">현재 {data.SHTER_NM}에서 보호 중</p>
+        </div>
+      ) : (
+        <Loading notext />
+      )}
     </TodayPanelStyle>
   );
 }
