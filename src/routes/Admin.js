5ad734245a91c114832d5379f4f68659251369af
soonzero <sykeum0912@naver.com>
2022-05-20 00:04:43 +0900

    docs: 불필요한 부분 제거

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { db } from "components/fbase/fbase";
 import Feed from "components/Feed";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Admin() {
   let didCancel = false;
