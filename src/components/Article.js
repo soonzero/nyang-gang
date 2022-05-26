8252ec392a479b82a8a3a5950539db0ecb5f6754
soonzero <sykeum0912@naver.com>
2022-05-26 18:05:37 +0900

    fix: 댓글 정렬 순서 오래된 순으로 아래로 정렬

--- src/components/Article.js
+++ src/components/Article.js
@@ -215,10 +215,10 @@ export default function Article(props) {
                 {props.comments[props.data.id] &&
                   props.comments[props.data.id]
                     .sort((a, b) => {
-                      if (a.time < b.time) {
+                      if (a.time > b.time) {
                         return 1;
                       }
-                      if (a.time > b.time) {
+                      if (a.time < b.time) {
                         return -1;
                       }
                       return 0;
